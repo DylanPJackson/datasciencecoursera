ec6a2730b0f078b9b124a2dacdc82739f9d14b12
DylanPJackson <frprdjackson@gmail.com>
2020-09-29 13:50:18 -0400

    Finished comments

--- r_prog_week2/pollutantmean.R
+++ r_prog_week2/pollutantmean.R
@@ -20,9 +20,11 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
     for (table_num in 1:size){
         # Get column index of specified pollutant
         col_ind <- which(colnames(tbl[[table_num]]) == pollutant) 
-        # Get vector of 
+        # Get vector of pollutant values 
         vals <- tbl[[table_num]][,col_ind][!is.na(tbl[[table_num]][,col_ind])]
+        # Add to the total sum 
         sum <- sum + sum(vals)
+        # Add to the total count 
         count <- count + length(vals)
     }
     return(sum / count)
